3a1e67f676b55c20e3109eee5739a5070accc799
Charles Hutchings <CHutchings@spartaglobal.com>
2019-06-03 11:03:26 +0100

    initial commit

---
+++ standard_library.py
@@ -0,0 +1,8 @@
+# Libraries
+
+# These are standard libraries which are always included in python.
+# To use them, they simply need to be imported as so...
+
+import random
+
+print(random.random())
